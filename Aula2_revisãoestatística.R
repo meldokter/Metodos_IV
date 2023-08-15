dec4951594bc94d379d9785927e91f394749c3f4
Mel Dokter Palomo <141670749+meldokter@users.noreply.github.com>
2023-08-15 17:28:04 -0300

    Add files via upload

---
+++ Aula2_revisãoestatística.R
@@ -0,0 +1,13 @@
+#1.Calculo de medias 
+
+x <- c(1,2,3,4,5,6,7,8,9,10)
+(media_x <- sum(x)/length(x))
+x <- c(5,5,5,5,5,5,5)
+(media_x <-  sum(x)/length(x))
+x <- c(1,3,5,7,9,11)
+(media_x <-  sum(x)/length(x))
+x <- c(-5,-4,-3,-2,-1,1,2,3,4,5)
+(media_x <-  sum(x)/length(x))
+
+# ou podemos simplemsnte usar mean(x)
+mean(x)
