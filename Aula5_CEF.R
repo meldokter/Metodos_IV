7fd5ec3713ada850a56c649fe6ffab3fc50a7217
Mel Dokter Palomo <141670749+meldokter@users.noreply.github.com>
2023-10-14 17:08:43 -0300

    Add files via upload

--- Aula5_CEF.R
+++ Aula5_CEF.R
@@ -29,3 +29,51 @@ df <- data.frame(x=x, y=y)
 reg <-  lm ( y~ x , data= df) #regressão
 summary(reg) #estatísticas do modelo
 
+#6.3.3 O erro da CEF é não-correlacionado com X
+table2 <- df %>%
+  mutate(total_n = sum(V1028),
+         log_renda = log(renda)) %>%
+  group_by(genero) %>%
+  summarise(salario = round(weighted.mean(salario, w=V1028),2), freq=sum(V1028)/total_n)
+
+table2 %>%
+  head(10) %>%
+  kable()
+
+#6.4.1.1 Equação da Reta
+library(ggplot2)
+library(dplyr)
+set.seed(1234)
+n <- 1000
+x <- rnorm(n)
+u <- rnorm(n)
+y <- 2 + 1.3*x + u
+
+df <- data.frame(y=y, x=x)
+df %>%
+  ggplot(aes(x, y)) + geom_point() + geom_smooth(se=F, method="lm") +
+  geom_abline(slope= .5, intercept = 1, colour="red") +
+  geom_abline(slope= 3, intercept = 3, colour="green", size=1) +
+  geom_abline(slope= 0, intercept = 2, colour="grey", size=1)
+#No gráfico, vemos que a reta azul é melhor do que a verde e vermelha.
+
+#encontrando os parâmetros da fórmula 
+library(knitr)
+df %>%
+  summarise(cov_yx = cov(y,x),
+            var_x = var(x),
+            beta = cov_yx/var_x,
+            alpha = mean(y) - beta*mean(x)) %>%
+  kable(digits=3)
+#| cov_yx| var_x|  beta| alpha|
+#|------:|-----:|-----:|-----:|
+#|  1.349| 0.995| 1.356| 2.016|
+
+#Estimando a equação linear no R: função LM (linear model)
+lm(y ~x, df)
+#alpha:       beta    
+#2.016        1.356  
+
+#df nesse caso é o meu banco de dados 
+
+
