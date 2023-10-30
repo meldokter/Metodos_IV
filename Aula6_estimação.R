cf8807d8dbcccc0c62aac0ed92d2e718b0535cb9
Mel Dokter Palomo <141670749+meldokter@users.noreply.github.com>
2023-10-30 19:16:11 -0300

    Add files via upload

--- Aula6_estimação.R
+++ Aula6_estimação.R
@@ -122,7 +122,8 @@ rho <- .15
 sigma_x <- 4
 sigma_y <- 2
 
-matriz_var_cov <- matrix(c(sigma_x^2, rho*sigma_x*sigma_y, rho*sigma_x*sigma_y,  sigma_y^2 ), byrow=T, nrow=2)
+matriz_var_cov <- matrix(c(sigma_x^2, rho*sigma_x*sigma_y, rho*sigma_x*sigma_y,  
+                           sigma_y^2 ), byrow=T, nrow=2)
 
 matriz_var_cov %>%
   kable()
@@ -130,7 +131,8 @@ matriz_var_cov %>%
 #|  1.2| 4.0|
 
 set.seed(345)
-norm_bivariada <- MASS::mvrnorm(n = 10000, mu = vetor_media, Sigma  = matriz_var_cov)
+norm_bivariada <- MASS::mvrnorm(n = 10000, mu = vetor_media, 
+                                Sigma  = matriz_var_cov)
 
 bivariada_tibble4 <- as_tibble(norm_bivariada, .name_repair = "universal") %>%
   rename(x = '...1', 
@@ -155,3 +157,92 @@ stargazer::stargazer(list(reg1, reg2, reg3, reg4), type =  "html",
                      column.labels=c("cov = 2.4, s_x = 2", "cov = 1.2, s_x = 2", 
                                      "cov = 2.4, s_x = 4", 
                                      "cov = 1.2, s_x = 4" ))
+
+#7.6 Estimando um modelo de regressão real-Censo da Educação
+
+# instalando a biblioteca
+install.packages('basedosdados')
+
+# carregando a biblioteca na sessão
+library(basedosdados)
+
+# para importar os dadosdiretamente no R, 
+#precisamos criar um id para acessar a base de dados
+set_billing_id("aula-reg-manoel")
+
+# checando que deu certo
+get_billing_id()
+
+# importando dados
+query <- "SELECT * FROM `basedosdados.br_inep_censo_escolar.escola` where sigla_uf = 'AL' and id_municipio = '2704302' and ano = 2019" 
+escola <- read_sql(query)
+
+query <- "SELECT count(*) as contagem, id_municipio FROM `basedosdados.br_inep_censo_escolar.escola` where sigla_uf = 'AL' and ano = 2019 group by id_municipio" 
+escola <- read_sql(query)
+
+query <- "SELECT * FROM `basedosdados.br_inep_censo_escolar.dicionario`"
+dicionario <- read_sql(query)
+
+query <- "SELECT * FROM `basedosdados.br_inep_censo_escolar.matricula` where sigla_uf = 'AL' and id_municipio = '2704302' and ano = 2019"
+
+aluno <- read_sql(query)
+
+# dicionário de gênero e raça
+# dicionario %>%
+#   dplyr::filter(id_tabela == "matricula", nome_coluna %in% c("sexo", "raca_cor"))
+
+
+aluno_gen <- aluno %>%
+  dplyr::filter(regular == 1) %>%
+  group_by(id_escola, sexo) %>%
+  summarise(num_aluno_gen = n()) %>%
+  mutate(sexo = gsub("1", "Male", sexo),
+         sexo = gsub("2", "Female", sexo)) %>%
+  mutate(total = sum(num_aluno_gen),
+         percent = num_aluno_gen/total) %>%
+  filter(sexo == "Female") %>%
+  rename(percent_female = percent) %>%
+  dplyr::select(id_escola, percent_female)
+
+aluno_raca <- aluno %>%
+  dplyr::filter(regular == 1) %>%
+  group_by(id_escola, raca_cor) %>%
+  summarise(num_aluno_raca = n()) %>%
+  mutate(raca_cor = gsub("1", "Branca", raca_cor),
+         raca_cor = gsub("2", "Preta", raca_cor),
+         raca_cor = gsub("3", "Parda", raca_cor)) %>%
+  mutate(total = sum(num_aluno_raca),
+         percent = num_aluno_raca/total) %>%
+  filter(raca_cor %in% c("Branca", "0", "Preta", "Parda")) %>%
+  mutate(raca_cor = gsub("0","não_declarado", raca_cor)) %>%
+  select(id_escola, raca_cor, percent) %>%
+  pivot_wider(names_from = raca_cor , values_from = percent )
+
+
+
+aluno_escola <- aluno_gen %>%
+  inner_join(aluno_raca,  by = "id_escola") %>%
+  inner_join(escola, by = "id_escola")
+
+aluno_escola_reg <- aluno_escola %>%
+  ungroup() %>%
+  mutate(negra = Preta + Parda) %>%
+  select(negra, percent_female, tipo_localizacao, agua_potavel, esgoto_rede_publica, 
+         lixo_servico_coleta, area_verde, biblioteca, quantidade_profissional_psicologo) %>%
+  filter(across(everything(), ~!is.na(.))) %>%
+  mutate_if(bit64::is.integer64, as.factor)
+
+reg <- lm(negra ~  percent_female + tipo_localizacao + agua_potavel  + esgoto_rede_publica + lixo_servico_coleta + area_verde + biblioteca + quantidade_profissional_psicologo, data= aluno_escola_reg)
+
+summary(reg)
+
+#limitações r-quadrado
+x1 <- rnorm(1000, 0, 10)
+x2 <- rnorm(1000, 0, 2)
+
+y1 <- 2 + 3*x1 + rnorm(1000, 0, 2)
+y2 <- 2 + 3*x2 + rnorm(1000, 0, 2)
+
+summary(lm(y1 ~x1))
+
+summary(lm(y2~x2))
