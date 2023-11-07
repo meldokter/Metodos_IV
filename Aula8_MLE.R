f1f23c3398ad858ce19c1e3d42e048fcb1f3d832
Mel Dokter Palomo <141670749+meldokter@users.noreply.github.com>
2023-11-07 17:37:54 -0300

    Add files via upload

--- Aula8_MLE.R
+++ Aula8_MLE.R
@@ -27,3 +27,37 @@ hist(rnorm(10000))
 40*.007*100 # 40 anos estimativa pontual
 40*.003*100 # 40 anos limite inferior
 40*.01*100 # 40 anos limite superior
+
+#aula 7/11 - regressão
+install.packages("data.table")
+library(data.table)
+
+#carregando os dados
+library(readr)
+BES <- read_csv("BES.csv")
+View(BES)
+bes <- BES
+
+#regressão
+reg <- lm(leave ~ age, data=bes, na.action = na.omit )
+
+stargazer::stargazer(reg, type =  "html", style = "ajps",
+                     title = "Regressão linear - Brexit", omit.stat = "f")
+summary(reg)
+
+#teste de hipótese 
+sim_nula <- rnorm(10000, mean = 0, sd= 0.0001739)
+options(scipen = 99)
+hist(sim_nula)
+#p-valor = <2e-16 =0 = rejeita-se HO de que B=0 
+
+#intervalo de confiança 
+0.0072082 + 2*0.0001739
+0.0072082 - 2*0.0001739
+#[0.0068604 - 0.007556]
+0.007556 - 0.0068604 # 0.0006956
+
+#IC para 40 anos
+0.1194782 + (0.0072082 + 0.0001739)*40
+0.1194782 + (0.0072082 - 0.0001739)*40
+#40% de confiança 
